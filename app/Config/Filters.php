84c36ec80eaf4d48acd658696b0223c6a576ee5f
shubham08-cs <me.shubhamsingh8998@gmail.com>
2026-02-03 10:43:57 +0530

    removed debugger

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -58,7 +58,6 @@ class Filters extends BaseFilters
         'after' => [
             'pagecache',   // Web Page Caching
             'performance', // Performance Metrics
-            'toolbar',     // Debug Toolbar
         ],
     ];
 
